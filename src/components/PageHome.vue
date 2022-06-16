dd64de8f33b67c6c6aff2041811b1ea4c5c35e65
mouradeljayi <mouradeljayi98@gmail.com>
2022-06-16 19:42:22 +0100

    first look

---
+++ src/components/PageHome.vue
@@ -0,0 +1,26 @@
+<template>
+   <h1>Welcome to the Forum</h1>
+    <ThreadList :threads="threads"/>
+</template>
+
+<script>
+import ThreadList from '@/components/ThreadList.vue'
+import sourceData from '@/data.json'
+
+export default {
+    name: 'PageHome',
+    components: {
+      ThreadList
+    },
+    data() {
+    return {
+      threads: sourceData.threads,
+    }
+  },
+  
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import sourceData from '@/data.json'
+
+import PageHome from '@/components/PageHome'
+import PageThreadShow from '@/components/PageThreadShow'
+import PageNotFound from '@/components/PageNotFound'
+
+
+const routes = [
+    {
+        path: '/',
+        component: PageHome,
+        name: 'Home'
+    },
+    {
+        path: '/thread/:id',
+        component: PageThreadShow,
+        name: 'ThreadShow',
+        props: true,
+        beforeEnter: (to, from, next) => {
+            const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+            if(threadExists) {
+                return next()
+            } else {
+                return next({ 
+                    name: 'NotFound',
+                    params: {pathMatch: to.path.substring(1).split('/')},
+                    query: to.query,
+                    hash: to.hash
+
+                })
+            }
+        }
+    },
+    { 
+        path: '/:pathMatch(.*)*', 
+        name: 'NotFound', 
+        component: PageNotFound 
+    },
+
+]
+
+export default createRouter({
+    history: createWebHistory(),
+    routes, 
+  })
